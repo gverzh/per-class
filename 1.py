5b9417c2b1e46d5466f5ee6e177a1b80be5539a0
gvezrh <525114095@qq.com>
2024-06-11 23:53:11 +0800

    '初始化项目版本4'

---
+++ 1.py
@@ -0,0 +1,11 @@
+y_min_eeg = 0
+y_max_eeg = 3
+y_min_imu = -500
+y_max_imu = 500
+
+RING_BUF_SIZE = 81920
+PLOT_FPS = 10
+N_MAX_POINTS_EEG = 1200
+N_MAX_POINTS_IMU = 1000
+IMU_ACCL_Y_MAX = 2
+IMU_ACCL_Y_MIN = -2
